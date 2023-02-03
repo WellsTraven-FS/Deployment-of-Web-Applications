7339fa008c0ad9601df496fee1c81475fe5b86bb
Traven Wells <104939327+WellsTraven-FS@users.noreply.github.com>
2023-02-03 15:19:14 -0500

    pushing up tailwind style and dependency

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./index.html",
+        "./blog1.html",
+        "./blog2.html",
+        "./blog3.html",
+        "./blog4.html",
+        "./blog5.html",
+        "./blog6.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
